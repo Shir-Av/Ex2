4ca7a77e81ae245632b40e7ab6a4df4aff4e85b5
Avraham <shirush771@gmail.com>
2019-12-22 19:16:15 +0200

    finish dataStructure

--- code/dataStructure/DGraph.java
+++ code/dataStructure/DGraph.java
@@ -1,73 +1,161 @@
 package dataStructure;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 public class DGraph implements graph{
+	public HashMap<Integer,node_data> nodes;
+	public HashMap<Integer,HashMap<Integer,edge_data>> graph;
+	private static int MC = 0;
+	private static int NODE_SIZE = 0;
+	private static int EDGE_SIZE = 0;
 
-	@Override
-	public node_data getNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
+	public DGraph()
+	{
+		HashMap<Integer,node_data> nodes = new HashMap<Integer,node_data>();
+		HashMap<Integer,HashMap<Integer,edge_data>> graph = new HashMap<Integer,HashMap<Integer,edge_data>>();
 	}
 
 	@Override
-	public edge_data getEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+	public node_data getNode(int key)
+	{
+		return nodes.get(key);
 	}
 
 	@Override
-	public void addNode(node_data n) {
-		// TODO Auto-generated method stub
+	public edge_data getEdge(int src, int dest)
+	{
+		if (src == dest)
+		{
+			return null;
+		}
+		else if (nodes.get(src) == null || nodes.get(dest) == null)
+		{
+			return null;
+		}
+		else return graph.get(src).get(dest);
+	}
+
+	@Override
+	public void addNode(node_data n)
+	{
+
+		nodes.put(n.getKey(), n);
+		NODE_SIZE++;
+		MC++;
 		
 	}
 
 	@Override
-	public void connect(int src, int dest, double w) {
-		// TODO Auto-generated method stub
-		
+	public void connect(int src, int dest, double w)
+	{
+		if (!nodes.containsKey(src) || !nodes.containsKey(dest))
+		{
+			System.out.println("ERROR: src/dest node does not exist");
+			return;
+		}
+		EdgeData e = new EdgeData((NodeData) nodes.get(src), (NodeData) nodes.get(dest), w);
+		HashMap<Integer,edge_data> temp = graph.get(src);
+		if(temp.values().isEmpty())
+		{
+			//HashMap<Integer,edge_data> temp = new HashMap<Integer,edge_data>();
+			temp.put(dest, e);
+			graph.put(src, temp);
+		}
+		else
+		{
+			graph.get(src).put(dest, e);
+		}
+		EDGE_SIZE++;
+		MC++;
+
 	}
 
 	@Override
-	public Collection<node_data> getV() {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<node_data> getV()
+	{
+		return this.nodes.values();
 	}
 
 	@Override
-	public Collection<edge_data> getE(int node_id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<edge_data> getE(int node_id)
+	{
+		if(nodes.get(node_id) == null)
+		{
+			System.out.println("ERROR: this node does not exist");
+			return null;
+		}
+		else if (this.graph.get(node_id).values().isEmpty())
+		{
+			System.out.println("ERROR: there are no edges getting out of this given node");
+			return null;
+		}
+		else return this.graph.get(node_id).values();
 	}
 
 	@Override
-	public node_data removeNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
+	public node_data removeNode(int key)
+	{
+		if (!nodes.containsKey(key))
+		{
+			System.out.println("ERROR: the given node does not exist");
+			return null;
+		}
+		else
+		{
+			if(!this.graph.get(key).values().isEmpty()){
+				EDGE_SIZE -= this.graph.get(key).values().size();
+				graph.remove(key);
+
+			}
+			for (HashMap<Integer,edge_data> temp: this.graph.values()) {
+				if(temp.containsKey(key))
+				{
+					temp.remove(key);
+					EDGE_SIZE--;
+
+				}
+			}
+		}
+		MC++;
+		NODE_SIZE--;
+		return nodes.remove(key);
 	}
 
 	@Override
-	public edge_data removeEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+	public edge_data removeEdge(int src, int dest)
+	{
+		if(this.graph.get(src).get(dest) == null)
+		{
+			System.out.println("ERROR: the given edge does not exist");
+			return null;
+		}
+		else
+		{
+			edge_data temp = this.graph.get(src).remove(dest);
+			EDGE_SIZE--;
+			MC++;
+			return temp;
+		}
+
 	}
 
 	@Override
-	public int nodeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int nodeSize()
+	{
+		return NODE_SIZE;
 	}
 
 	@Override
-	public int edgeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int edgeSize()
+	{
+		return EDGE_SIZE;
 	}
 
 	@Override
-	public int getMC() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getMC()
+	{
+		return MC;
 	}
 
 }
--- code/dataStructure/EdgeData.java
+++ code/dataStructure/EdgeData.java
@@ -1,38 +1,83 @@
 package dataStructure;
 
+import java.awt.*;
+
 public class EdgeData implements edge_data {
-    @Override
-    public int getSrc() {
-        return 0;
+    private NodeData src;
+    private NodeData dest;
+    private double eWeight;
+    private String info;
+    private Color tag;
+
+    public EdgeData ()
+    {
+        this.src = new NodeData();
+        this.dest = new NodeData();
+        this.eWeight = 0;
+        this.info = null;
+        this.tag = Color.BLACK;
+    }
+    public EdgeData (NodeData src, NodeData dest, double eWeight)
+    {
+        this.src = src;
+        this.dest = dest;
+        this.eWeight = eWeight;
+        this.info = null;
+        this.tag = Color.BLACK;
+    }
+    public EdgeData (EdgeData e)
+    {
+        this.src = new NodeData(e.src);
+        this.dest = new NodeData(e.dest);
+        this.eWeight = e.eWeight;
+        this.info = e.info;
+        this.tag = e.tag;
     }
 
     @Override
-    public int getDest() {
-        return 0;
+    public int getSrc()
+    {
+        return this.src.getKey();
     }
 
     @Override
-    public double getWeight() {
-        return 0;
+    public int getDest()
+    {
+        return this.dest.getKey();
     }
 
     @Override
-    public String getInfo() {
-        return null;
+    public double getWeight()
+    {
+        return this.eWeight;
     }
 
     @Override
-    public void setInfo(String s) {
-
+    public String getInfo()
+    {
+        return this.info;
     }
 
     @Override
-    public int getTag() {
-        return 0;
+    public void setInfo(String s)
+    {
+        this.info = s;
     }
 
     @Override
-    public void setTag(int t) {
+    public int getTag()
+    {
+        return this.tag.getRGB();
+    }
 
+    @Override
+    public void setTag(int t)
+    {
+        this.tag = new Color(t);
+    }
+    public String toString()
+    {
+        String s = "Src: "+ this.src.getKey() +"dest: "+ this.dest.getKey() + " , Weight: "+ this.eWeight + " , Info: "+ this.info + " , Tag: "+ this.tag;
+        return s;
     }
 }
